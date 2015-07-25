b627815d288191b18203e577b5b8b0191ff67334
rvegas <ricardo.vegas@critizen.com>
2015-07-25 19:17:00 +0200

    changing to 8080

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from elasticsearch import Elasticsearch
 app = Flask(__name__)
+es = Elasticsearch()
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
