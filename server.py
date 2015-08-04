8ac21b790ba7b230ff9b387cdc96946c0b4be5c3
trippr <ricardovegas@gmail.com>
2015-08-04 07:34:39 -0400

    mend

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from elasticsearch import Elasticsearch
+import re
+import random
+
+
 app = Flask(__name__)
 es = Elasticsearch([
     # 'http://ec2-52-25-78-6.us-west-2.compute.amazonaws.com:9200',
@@ -10,18 +14,22 @@ es = Elasticsearch([
 @app.route('/', methods=['POST'])
 def hello_world():
     text = str(request.form['text'])
-    print(text)
+    # print(text)
     excluded = {}
     if 'excluded' in request.form:
         excluded = request.form['excluded'].split(',')
 
+    splitted = re.split(',| ', text)
+    sample = random.sample(splitted, len(splitted)/8)
+    sampled_text = ' '.join(sample)
+    print(sampled_text)
     print(excluded)
     result = es.search(
         index='trippr',
         body={
             "query": {
                 "more_like_this": {
-                    "like_text": text,
+                    "like_text": sampled_text,
                     "min_term_freq": 1,
                     "min_doc_freq": 1
                 }
@@ -33,7 +41,7 @@ def hello_world():
     print("Got %d Hits:" % total)
 
     hits = result['hits']['hits']
-    city = 'error'
+    city = ''
 
     for hit in hits:
         if hit['_source']['name'] in excluded:
@@ -42,11 +50,13 @@ def hello_world():
             city = hit['_source']['name'] \
                 + ':' \
                 + hit['_source']['country'] \
-                + ':' \
-                + hit['_source']['countrycode']
+		+ ':' \
+		+ hit['_source']['countrycode'] \
+		+ ':' \
+		+ hit['_source']['airportcode']
 
     print city
     return city
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
